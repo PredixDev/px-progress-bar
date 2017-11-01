14c7f502a3b276a593decff86d4625c5bc3c1761
David Leonard <davidrobertleonard@gmail.com>
2017-11-01 14:09:38 -0700

    Update tests to Polymer 2 syntax, add variants, other test-related cleanup

--- test/px-progress-bar-tests.js
+++ test/px-progress-bar-tests.js
@@ -1,86 +1,31 @@
-document.addEventListener("WebComponentsReady", function() {
-  runCustomTests();
+describe('progress bar with value below range', () => {
+  it('does not fill the progress bar', () => {
+    const progressBar = fixture('ValueBelowRange');
+    const fill = Polymer.dom(progressBar.root).querySelector('#fill');
+    expect(fill.style.transform).to.equal('scaleX(0)');
+  });
 });
 
-// This is the wrapper for custom its, called upon web components ready state
-function runCustomTests() {
-  // Place any setup steps like variable declaration and initialization here
+describe('progress bar with value in range', () => {
+  it('fills some of the progress bar', () => {
+    const progressBar = fixture('ValueInRange');
+    const fill = Polymer.dom(progressBar.root).querySelector('#fill');
+    expect(fill.style.transform).to.equal('scaleX(0.8)');
+  });
+});
 
-  // This is the placeholder describe to place custom its in
-  // Use itCase(options) for a more convenient setup of the it cases
-  describe('px_progress_bar_1 is created and initialized correctly', function() {
-    var progressBarFixture;
-    beforeEach(function() {
-      progressBarFixture = fixture('ValueBelowRange');
-    });
-    it('Check that fixture is created', function(done){
-      assert.isDefined(progressBarFixture);
-      done();
-    });
-    it('Check that px_progress_bar_1 has Value attribute', function(done){
-      assert.isDefined(progressBarFixture.value);
-      done();
-    });
-    it('Check initial value of progress bar', function(done){
-      assert.equal(progressBarFixture.value, '-10');
-      done();
-    });
-    it('Check width of progress bar fill', function(done){
-      assert.equal(progressBarFixture.$$('#fill').style.transform, 'scaleX(0)');
-      done();
-    });
+describe('progress bar with infinite value', () => {
+  it('fills the entire progress bar during the animation', () => {
+    const progressBar = fixture('ValueInfinite');
+    const fill = Polymer.dom(progressBar.root).querySelector('#fill');
+    expect(fill.style.transform).to.equal('scaleX(1)');
   });
-  describe('px_progress_bar_2 is created and initialized correctly', function() {
-    var progressBarFixture;
-    beforeEach(function() {
-      progressBarFixture = fixture('ValueInRange');
-    });
-    it('Check that fixture is created', function(done){
-      assert.isDefined(progressBarFixture);
-      done();
-    });
-    it('Check that px_progress_bar_2 has Value attribute', function(done){
-      assert.isDefined(progressBarFixture.value);
-      done();
-    });
-    it('Check initial value of progress bar', function(done){
-      assert.equal(progressBarFixture.value, '80');
-      done();
-    });
-    it('Check width of progress bar fill', function(done){
-      assert.equal(progressBarFixture.$$('#fill').style.transform, 'scaleX(0.8)');
-      done();
-    });
+});
+
+describe('progress bar with custom min and max', () => {
+  it('calculates the right ratio based on the min, max, and value', () => {
+    const progressBar = fixture('CustomMinMax');
+    const ratio = progressBar._ratio.toFixed(2);
+    expect(ratio).to.equal('0.79');
   });
-  describe('px_progress_bar_3 is created and initialized correctly', function() {
-    var progressBarFixture;
-    beforeEach(function() {
-      progressBarFixture = fixture('ValueInfinite');
-    });
-    it('Check that fixture is created', function(done){
-      assert.isDefined(progressBarFixture);
-      done();
-    });
-    it('Check that px_progress_bar_3 has Value attribute', function(done){
-      assert.isDefined(progressBarFixture.value);
-      done();
-    });
-    it('Check initial value of progress bar', function(done){
-      assert.equal(progressBarFixture.value, '120');
-      done();
-    });
-    it('Check width of progress bar fill', function(done){
-      assert.equal(progressBarFixture.$$('#fill').style.transform, 'scaleX(1)');
-      done();
-    });
-  });
-  describe('custom min and max', function() {
-    var progressBarFixture;
-    beforeEach(function() {
-      progressBarFixture = fixture('CustomMinMax');
-    });
-    it('calculates the right ratio based on the min, max, and value', function() {
-      assert.equal(progressBarFixture._ratio.toFixed(2), '0.79');
-    });
-  });
-}
+});
